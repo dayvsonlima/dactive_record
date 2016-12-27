ae3856bd6d5b2e10b41e36bfb8ad69f95632ee5a
dayvsonlima <dayvsonlima31@gmail.com>
2016-12-27 11:28:49 -0300

    Fix require

--- lib/dactive_record.rb
+++ lib/dactive_record.rb
@@ -1,6 +1,6 @@
 module DactiveRecord
 end
 
-require 'dactive_record/query_buidlers/dynamodb'
+require 'dactive_record/query_builders/dynamodb'
 require 'dactive_record/adapters/dynamodb'
 require 'dactive_record/base'
